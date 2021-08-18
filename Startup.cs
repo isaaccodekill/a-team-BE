baa25a2c97f9d1240a682240c7795b4156a1b334
Isaac_voyance <isaacbello3200@gmail.com>
2021-08-18 18:45:59 +0100

    ssl test 2

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace Searchify
                     var pgPass = pgUserPass.Split(":")[1];
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}?sslmode=require";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL=true;SslMode=Require;";
                 
 
                 }
